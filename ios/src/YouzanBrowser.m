ba994fc3744f850c5825eb463836c18b23481ef1
yangbiwan <yangbiwan@lasoplanet.com>
2021-03-02 12:12:22 +0800

    create YZWebView

--- ios/src/YouzanBrowser.m
+++ ios/src/YouzanBrowser.m
@@ -20,15 +20,22 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-        NSURL *url = [NSURL URLWithString:[self.source objectForKey:@"uri"]];
-        NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
-        [self.webView loadRequest:urlRequest];
+        self.webView = [[YZWebView alloc]initWithWebViewType:YZWebViewTypeWKWebView];
+        self.webView.delegate = self;
+        self.webView.noticeDelegate = self;
     }
     return self;
 }
 
 - (void)setSource:(NSDictionary *)source {
-    self.source = source;
+    if (![_source isEqualToDictionary:source]) {
+        _source = [source copy];
+        if (_webView != nil) {
+            NSURL *url = [NSURL URLWithString:[_source objectForKey:@"uri"]];
+            NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+            [_webView loadRequest:urlRequest];
+        }
+    }
 }
 
 - (NSArray<NSString *> *)supportedEvents {
@@ -91,51 +98,70 @@
     }];
 }
 
-#pragma mark Send Events
+#pragma mark YZWebViewDelegate
+
+- (BOOL)webView:(id<YZWebView>)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(WKNavigationType)navigationType {
+    NSLog(@"shouldStartLoadWithRequest");
+    return YES;
+}
+- (void)webViewDidStartLoad:(id<YZWebView>)webView {
+    NSLog(@"webViewDidStartLoad");
+}
+- (void)webViewDidFinishLoad:(id<YZWebView>)webView {
+    NSLog(@"webViewDidFinishLoad");
+}
+- (void)webView:(id<YZWebView>)webView didFailLoadWithError:(NSError *)error {
+    NSLog(@"didFailLoadWithError");
+}
+- (void)webViewWebContentProcessDidTerminate:(id<YZWebView>)webView {
+    NSLog(@"webViewWebContentProcessDidTerminate");
+}
+
+#pragma mark YZWebViewNoticeDelegate
 
 - (void)webView:(nonnull id<YZWebView>)webView didReceiveNotice:(nonnull YZNotice *)notice {
     switch (notice.type) {
             // 收到登陆请求
         case YZNoticeTypeLogin: {
             NSLog(@"需要登陆 --- %@", notice.response);
-            [self sendEventWithName:@"yzObserveLogin" body:notice.response];
+//            [self sendEventWithName:@"yzObserveLogin" body:notice.response];
             break;
         }
             // 收到分享的回调数据
         case YZNoticeTypeShare: {
             NSLog(@"收到分享的回调数据 --- %@", notice.response);
-            [self sendEventWithName:@"yzObserveShare" body:notice.response];
+//            [self sendEventWithName:@"yzObserveShare" body:notice.response];
             break;
         }
             // Web页面已准备好
             // 此时可以分享，但注意此事件并不作为是否可分享的标志事件
         case YZNoticeTypeReady: {
             NSLog(@"Web页面已准备好 --- %@", notice.response);
-            [self sendEventWithName:@"yzObserveReady" body:notice.response];
+//            [self sendEventWithName:@"yzObserveReady" body:notice.response];
             break;
         }
             // 加入购物车的时候调用
         case YZNoticeTypeAddToCart: {
             NSLog(@"购物车 --- %@", notice.response);
-            [self sendEventWithName:@"yzObserveAddToCart" body:notice.response];
+//            [self sendEventWithName:@"yzObserveAddToCart" body:notice.response];
             break;
         }
             // 立即购买
         case YZNoticeTypeBuyNow: {
             NSLog(@"立即购买 -- %@", notice.response);
-            [self sendEventWithName:@"yzObserveBuyNow" body:notice.response];
+//            [self sendEventWithName:@"yzObserveBuyNow" body:notice.response];
             break;
         }
             // 购物车结算时调用
         case YZNoticeTypeAddUp: {
             NSLog(@"购物车结算 --- %@", notice.response);
-            [self sendEventWithName:@"yzObserveAddUp" body:notice.response];
+//            [self sendEventWithName:@"yzObserveAddUp" body:notice.response];
             break;
         }
             // 支付成功回调结果页
         case YZNoticeTypePaymentFinished: {
             NSLog(@"支付成功回调结果页 --- %@", notice.response);
-            [self sendEventWithName:@"yzObservePaymentFinished" body:notice.response];
+//            [self sendEventWithName:@"yzObservePaymentFinished" body:notice.response];
             break;
         }
         default:
