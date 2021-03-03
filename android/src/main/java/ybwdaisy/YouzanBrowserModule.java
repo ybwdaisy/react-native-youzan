093880608664b78f20dd14f515513e9dcb4cca83
yangbiwan <yangbiwan@lasoplanet.com>
2021-03-03 16:03:50 +0800

    feat: android add login and logout methods

--- android/src/main/java/ybwdaisy/YouzanBrowserModule.java
+++ android/src/main/java/ybwdaisy/YouzanBrowserModule.java
@@ -1,9 +1,15 @@
 package ybwdaisy;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.youzan.androidsdk.YouzanSDK;
+import com.youzan.androidsdk.YouzanToken;
+import com.youzan.androidsdk.YzLoginCallback;
+
+import java.util.HashMap;
 
 public class YouzanBrowserModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +26,27 @@ public class YouzanBrowserModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void login(ReadableMap info, final Promise promise) {
+        YouzanSDK.yzlogin(info.getString("openUserId"),  info.getString("avatar"),  info.getString("extra"), info.getString("nickName"), info.getString("gender"), new YzLoginCallback() {
+            @Override
+            public void onSuccess(YouzanToken youzanToken) {
+                HashMap res = new HashMap();
+                res.put("status", 0);
+                res.put("yzOpenId", youzanToken.getYzOpenId());
+                promise.resolve(res);
+            }
+            @Override
+            public void onFail(String s) {
+                promise.reject("-1", "登录失败");
+            }
+        });
+    }
+
+    @ReactMethod
+    public void logout(final Promise promise) {
+        YouzanSDK.userLogout(reactContext);
+        HashMap res = new HashMap();
+        res.put("status", 0);
+        promise.resolve(res);
     }
 }
